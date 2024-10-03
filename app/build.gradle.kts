26c6b914128f3d60e93ed1b2764905b6271abb2a
Rianda <e41231605@student.polije.ac.id>
2024-10-03 20:10:12 +0700

    build gradle kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,50 +1,55 @@
 plugins {
-    alias(libs.plugins.android.application) // Apply the Android application plugin
+    alias(libs.plugins.android.application)
 }
 
 android {
-    namespace = "com.example.jsonmoviedb" // Specify the application namespace
-    compileSdk = 34 // Compile SDK version
+    namespace = "com.example.jsonmoviedb"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.jsonmoviedb" // Unique application ID
-        minSdk = 24 // Minimum SDK version supported
-        targetSdk = 34 // Target SDK version
-        versionCode = 1 // Application version code
-        versionName = "1.0" // Application version name
+        applicationId = "com.example.jsonmoviedb"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner" // Test runner for instrumentation tests
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false // Disable code shrinking
+            isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"), // Default ProGuard configuration
-                "proguard-rules.pro" // Custom ProGuard rules
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
             )
         }
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8 // Set source compatibility to Java 8
-        targetCompatibility = JavaVersion.VERSION_1_8 // Set target compatibility to Java 8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
 dependencies {
-    implementation(libs.appcompat) // AndroidX AppCompat library for backward compatibility
-    implementation(libs.material) // Material Design components
-    implementation(libs.activity) // Activity KTX library
-    implementation("com.amitshekhar.android:android-networking:1.0.2") // Networking library
-    implementation("com.remotion.cardslider:card-slider:0.3.1") // Card Slider library
-    implementation("com.github.ivbaranov:materialfavoritebutton:0.1.5") // Material favorite button library
-    implementation("com.github.florent37:diagonallayout:1.0.9") // Diagonal layout library
-    implementation("com.flaviofaria:kenburnsview:1.0.7") // Ken Burns view library
-    implementation("com.github.bumptech.glide:glide:4.11.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.11.0")
-    implementation(libs.constraintlayout) // ConstraintLayout for flexible UI design
-    testImplementation(libs.junit) // JUnit library for unit testing
-    androidTestImplementation(libs.ext.junit) // JUnit extensions for Android testing
-    androidTestImplementation(libs.espresso.core) // Espresso library for UI testing
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.common)
+
+    // Dependency untuk Android Networking
+    implementation ("com.amitshekhar.android:android-networking:1.0.2")
+
+    // RecyclerView
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
+    implementation(libs.transport.api)
+
+    // Unit Testing
+    testImplementation(libs.junit)
+
+    // Android Testing
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 }
