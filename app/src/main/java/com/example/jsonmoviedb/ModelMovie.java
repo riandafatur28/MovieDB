f7ea020d41b05c361280adee8eb51bb5c476fdbf
Rianda <e41231605@student.polije.ac.id>
2024-10-03 20:12:13 +0700

    coding model view java

---
+++ app/src/main/java/com/example/jsonmoviedb/ModelMovie.java
@@ -0,0 +1,81 @@
+package com.example.jsonmoviedb;
+
+import java.io.Serializable;
+
+public class ModelMovie implements Serializable {
+
+    private int id;
+    private String title;
+    private double voteAverage;
+    private String overview;
+    private String releaseDate;
+    private String posterPath;
+    private String backdropPath;
+    private String popularity;
+
+    public ModelMovie() { }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public String getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(String popularity) {
+        this.popularity = popularity;
+    }
+}
