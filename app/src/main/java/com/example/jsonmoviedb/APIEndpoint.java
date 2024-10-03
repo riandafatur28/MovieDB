1629f1bd70f2a4f2a4c06ce9a0e26de39c3426a0
Rianda <e41231605@student.polije.ac.id>
2024-10-03 20:09:57 +0700

    coding ApiEndpoint

---
+++ app/src/main/java/com/example/jsonmoviedb/APIEndpoint.java
@@ -0,0 +1,12 @@
+package com.example.jsonmoviedb;
+
+class ApiEndpoint {
+    public static String BASEURL = "https://api.themoviedb.org/3/";
+    public static String APIKEY = "api_key-be7876d5db5c50eefc77abf419b1f064 ";
+    public static String LANGUAGE = "&language=en-US";
+    public static String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String NOW_PLAYING = "movie/now_playing?";
+    public static String MOVIE_POPULAR = "discover/movie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
+}
