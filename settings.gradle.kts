e17578fae474caf5501c9b02c0556a0ec73496b7
Rianda <e41231605@student.polije.ac.id>
2024-10-03 20:08:19 +0700

    setting gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,16 +9,18 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") } // Include JitPack
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") } // Include JitPack here as well
     }
 }
 
 rootProject.name = "JsonMovieDB"
 include(":app")
- 
