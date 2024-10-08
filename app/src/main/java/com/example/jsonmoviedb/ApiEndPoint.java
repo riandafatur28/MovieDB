035e6dcba7310d7f0867943c7e6776cf4237f5d2
IlmanNafi11 <ilmannafi04@gmail.com>
2024-10-08 08:22:34 +0700

    configurasi end point api

--- app/src/main/java/com/example/jsonmoviedb/ApiEndPoint.java
+++ app/src/main/java/com/example/jsonmoviedb/ApiEndPoint.java
@@ -1,12 +1,12 @@
 package com.example.jsonmoviedb;
 
-class ApiEndpoint {
+public class ApiEndPoint {
     public static String BASEURL = "https://api.themoviedb.org/3/";
-    public static String APIKEY = "api_key-be7876d5db5c50eefc77abf419b1f064 ";
+    public static String APIKEY = "api_key=ddb335a75ea4eb8af10b36fce9df8dc0";
     public static String LANGUAGE = "&language=en-US";
     public static String SEARCH_MOVIE = "search/movie?";
     public static String QUERY = "&query=";
-    public static String NOW_PLAYING = "movie/now_playing?";
+    public static String MOVIE_PLAYNOW = "movie/now_playing?";
     public static String MOVIE_POPULAR = "discover/movie?";
-    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg";
 }
